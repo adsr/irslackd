82e6a91b1cabedbcbc24a753c2ab6d33b162203d
Adam Saponara <as@php.net>
2018-05-16 12:33:33 -0400

    Prep Irslackd class for easier injectability

--- bin/irslackd.js
+++ bin/irslackd.js
@@ -5,11 +5,11 @@ const os = require('os');
 const fs = require('fs');
 const irslackd = require('../lib/irslackd.js');
 
-new irslackd.Irslackd().run({
+new irslackd.Irslackd({
   host: process.env.IRSLACKD_LISTEN_HOST || '0.0.0.0',
   port: process.env.IRSLACKD_LISTEN_PORT || 6697,
   tlsOpts: {
     key: fs.readFileSync(process.env.IRSLACKD_TLS_PKEY || (os.homedir() + '/.irslackd/pkey.pem')),
     cert: fs.readFileSync(process.env.IRSLACKD_TLS_CERT || (os.homedir() + '/.irslackd/cert.pem')),
   },
-});
+}).listen();
