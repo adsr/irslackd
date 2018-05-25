b404d2d3afd7ae5de11db58759ff95f34275c52f
Adam Saponara <as@php.net>
2018-05-24 21:56:34 -0400

    Label fingerprint output

--- bin/create_tls_key.sh
+++ bin/create_tls_key.sh
@@ -4,4 +4,4 @@ set -xe
 mkdir -p ~/.irslackd
 openssl req -newkey rsa:4096 -nodes -sha512 -x509 -days 3650 -nodes -out ~/.irslackd/cert.pem -keyout ~/.irslackd/pkey.pem
 fingerprint=$(openssl x509 -noout -fingerprint -sha512 -inform pem -in ~/.irslackd/cert.pem | cut -d= -f2-)
-echo $fingerprint | tr -d ':'
+echo -e "\nFingerprint: $(echo $fingerprint | tr -d ':')"
