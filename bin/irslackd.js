2450d6be39b50977bc78e549a215f5953ab9cfeb
Adam Saponara <as@php.net>
2018-06-02 00:20:08 -0400

    Use cli switches instead of env vars (#29)

--- bin/irslackd.js
+++ bin/irslackd.js
@@ -5,11 +5,25 @@ const os = require('os');
 const fs = require('fs');
 const irslackd = require('../lib/irslackd.js');
 
+const DEFAULT_HOST = '127.0.0.1';
+const DEFAULT_PORT = 6697;
+const DEFAULT_TLS_PKEY = os.homedir() + '/.irslackd/pkey.pem';
+const DEFAULT_TLS_CERT = os.homedir() + '/.irslackd/cert.pem';
+
+const opt = require('node-getopt').create([
+  [ 'h', 'help',         'Show this help' ],
+  [ 'p', 'port=PORT',    'Set listen port (default: ' + DEFAULT_PORT + ')' ],
+  [ 'a', 'host=ADDR',    'Set listen address (default: ' + DEFAULT_HOST + ')' ],
+  [ 'k', 'privkey=PATH', 'Set TLS private key path (default: ' + DEFAULT_TLS_PKEY + ')' ],
+  [ 'c', 'cert=PATH',    'Set TLS cert path (default: ' + DEFAULT_TLS_CERT + ')' ],
+  [ 'i', 'insecure',     'Do not use TLS encryption (not recommended)' ],
+]).bindHelp().parseSystem();
+
 new irslackd.Irslackd({
-  host: process.env.IRSLACKD_LISTEN_HOST || '127.0.0.1',
-  port: process.env.IRSLACKD_LISTEN_PORT || 6697,
-  tlsOpts: {
-    key: fs.readFileSync(process.env.IRSLACKD_TLS_PKEY || (os.homedir() + '/.irslackd/pkey.pem')),
-    cert: fs.readFileSync(process.env.IRSLACKD_TLS_CERT || (os.homedir() + '/.irslackd/cert.pem')),
+  host: opt.options.host || DEFAULT_HOST,
+  port: opt.options.port || DEFAULT_PORT,
+  tlsOpts: opt.options.insecure ? false : {
+    key: fs.readFileSync(opt.options.privkey || DEFAULT_TLS_PKEY),
+    cert: fs.readFileSync(opt.options.cert || DEFAULT_TLS_CERT),
   },
 }).listen();
