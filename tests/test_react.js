9cc060969af76acfe6b5d89b5e812630bc7ed5aa
Adam Saponara <as@php.net>
2018-07-24 11:01:38 -0400

    Edit reaction format

--- tests/test_react.js
+++ tests/test_react.js
@@ -6,7 +6,7 @@ const mocks = require('./mocks');
 test('slack_react', async(t) => {
   t.plan(1 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
-  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :' + String.fromCharCode(1) + 'ACTION reacts @ test_slack_barr: sunglasses' + String.fromCharCode(1));
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :' + String.fromCharCode(1) + 'ACTION reacts @ test_slack_barr :sunglasses:' + String.fromCharCode(1));
   await c.daemon.onSlackReactionAdded(c.ircUser, {
     type: 'reaction_added',
     user: 'U1234USER',
