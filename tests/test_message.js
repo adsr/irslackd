b2fc3687e6be0629b22d5d009d62c97d56aa6869
Adam Saponara <as@php.net>
2018-10-10 09:38:11 -0400

    Prevent attempting slackizeText mid-word

--- tests/test_message.js
+++ tests/test_message.js
@@ -81,3 +81,49 @@ test('slack_privmsg', async(t) => {
   });
   t.end();
 });
+
+test('slack_ircize_text', async(t) => {
+  t.plan(1 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t);
+  c.slackWeb.expect('chat.postMessage', {
+    channel: 'C1234CHAN1',
+    text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
+    as_user: true,
+    thread_ts: null,
+  }, {
+    ok: true,
+    channel: 'C1234CHAN1',
+    ts: '1234.5678',
+  });
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :hello @test_slack_bazz in #test_chan_1');
+  await c.daemon.onSlackMessage(c.ircUser, {
+    text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
+    user: 'U1234USER',
+    channel: 'C1234CHAN1',
+    ts: '1234.5678',
+  });
+  t.end();
+});
+
+test('irc_slackize_text', async(t) => {
+  t.plan(1 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t);
+  c.slackWeb.expect('chat.postMessage', {
+    channel: 'C1234CHAN1',
+    text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
+    as_user: true,
+    thread_ts: null,
+  }, {
+    ok: true,
+    channel: 'C1234CHAN1',
+    ts: '1234.5678',
+  });
+  await c.daemon.onIrcPrivmsg(c.ircUser, { args: [ '#test_chan_1', 'hello @test_slack_bazz in #test_chan_1' ] });
+  await c.daemon.onSlackMessage(c.ircUser, {
+    text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
+    user: 'U1234USER',
+    channel: 'C1234CHAN1',
+    ts: '1234.5678',
+  });
+  t.end();
+});
