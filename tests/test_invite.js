58591c33a95b6a53299dd9b250836b73e04c806a
Jay Colson <jay@karma.net>
2019-02-08 11:13:23 +0000

    formatting changes, lint made me do it

---
+++ tests/test_invite.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const test = require('tape');
+const mocks = require('./mocks');
+
+test('irc_list', async(t) => {
+  t.plan(5 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t);
+  c.slackWeb.expect('channel.invite', { exclude_archived: true, types: 'public_channel', limit: 1000 }, {
+    ok: true,
+    channels: [
+      { name: 'chan1', num_members: 1, topic: { value: 'chan1 topic' } },
+      { name: 'chan2', num_members: 2, topic: { value: 'chan2 topic' } },
+      { name: 'chan3', num_members: 3, topic: { value: 'chan3 topic' } },
+    ],
+  });
+  c.ircSocket.expect(':irslackd 322 test_slack_user #chan1 1 :chan1 topic');
+  c.ircSocket.expect(':irslackd 322 test_slack_user #chan2 2 :chan2 topic');
+  c.ircSocket.expect(':irslackd 322 test_slack_user #chan3 3 :chan3 topic');
+  c.ircSocket.expect(':irslackd 323 test_slack_user :End of LIST');
+  await c.daemon.onIrcInvite(c.ircUser, { args: ['U1234USER', 'CFOOBAR'] });
+  t.end();
+});
