664d07b33f766a235f0c8d4b83c2a62b8514f80e
Jay Colson <jay@karma.net>
2019-02-09 17:52:04 +0000

    changed plan number

--- tests/test_invite.js
+++ tests/test_invite.js
@@ -4,16 +4,16 @@ const test = require('tape');
 const mocks = require('./mocks');
 
 test('irc_invite', async(t) => {
-  t.plan(mocks.connectOneIrcClient.planCount);
+  t.plan(1 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
-  c.slackWeb.expect('channel.invite', { users: 'U1234USER',
+  c.slackWeb.expect('conversations.invite', { users: 'U1235BARR',
     channel: 'CFOOBAR'}, {
     ok: true,
     channel: [
       { id: 'CFOOBAR' },
     ],
   });
-  c.ircSocket.expect('jay has invited U1234USER to CFOOBAR');
-  await c.daemon.onIrcInvite(c.ircUser, { args: ['U1234USER', 'CFOOBAR'] });
+  c.ircSocket.expect('jay has invited U1235BARR to CFOOBAR');
+  await c.daemon.onIrcInvite(c.ircUser, { args: ['U1235BARR', 'CFOOBAR'] });
   t.end();
 });
