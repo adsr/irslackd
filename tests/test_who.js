e71221b385ac033489e6166fba0fc26508f803e7
Jay Colson <jay@karma.net>
2019-02-26 11:33:03 +0000

    test_who is working now.  changed the async arrow function foreach implementation to a for of in order to make sure responses are back prior to test finishing.

--- tests/test_who.js
+++ tests/test_who.js
@@ -4,12 +4,12 @@ const test = require('tape');
 const mocks = require('./mocks');
 
 test('irc_who', async(t) => {
-  t.plan(4 + mocks.connectOneIrcClient.planCount);
+  t.plan(3 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
   c.slackWeb.expect('users.info', { user: 'U1234USER' }, {
     ok: true,
     user: {
-      name: 'fun_user',
+      name: 'test_slack_user',
       id: 'U1234USER',
       profile: [
         {email: 'foo@example.com',
@@ -17,22 +17,9 @@ test('irc_who', async(t) => {
       ],
     },
   });
-  c.slackWeb.expect('users.list', {
-    presence: false,
-    limit: 1000}, {
-    ok: true,
-    members: [
-      { name: 'fun_user',
-        id: 'U1234USER',
-        profile: [
-          {email: 'foo@example.com',
-            real_name: 'Foo Bar' },
-        ]},
-    ],
-  });
-  c.ircSocket.expect(':irslackd 352 test_slack_user # U1234USER irslackd api.slack.com fun_user G :0 Nobody');
-  c.ircSocket.expect(':irslackd 315 test_slack_user fun_user :End of WHO list');
-  await c.ircUser.mapIrcToSlack('fun_user', 'U1234USER');
-  await c.daemon.onIrcWho(c.ircUser, { args: ['fun_user'] });
+  c.ircSocket.expect(':irslackd 352 test_slack_user # U1234USER irslackd api.slack.com test_slack_user G :0 Nobody');
+  c.ircSocket.expect(':irslackd 315 test_slack_user test_slack_user :End of WHO list');
+  await c.ircUser.mapIrcToSlack('test_slack_user', 'U1234USER');
+  await c.daemon.onIrcWho(c.ircUser, { args: ['test_slack_user'] });
   t.end();
 });
