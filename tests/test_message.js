033e2e500e79c8d50b3764db2fc9ee1b08f4fa93
Adam Saponara <as@php.net>
2019-07-09 14:48:05 -0400

    Ignore message_replied events (#78) and fix some other stuff

--- tests/test_message.js
+++ tests/test_message.js
@@ -62,16 +62,6 @@ test('irc_privmsg_with_err', async(t) => {
 test('slack_privmsg', async(t) => {
   t.plan(1 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
-  c.slackWeb.expect('chat.postMessage', {
-    channel: 'C1234CHAN1',
-    text: 'hello world',
-    as_user: true,
-    thread_ts: null,
-  }, {
-    ok: true,
-    channel: 'C1234CHAN1',
-    ts: '1234.5678',
-  });
   c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :hello world');
   await c.daemon.onSlackMessage(c.ircUser, {
     text: 'hello world',
@@ -85,16 +75,6 @@ test('slack_privmsg', async(t) => {
 test('slack_ircize_text', async(t) => {
   t.plan(1 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
-  c.slackWeb.expect('chat.postMessage', {
-    channel: 'C1234CHAN1',
-    text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
-    as_user: true,
-    thread_ts: null,
-  }, {
-    ok: true,
-    channel: 'C1234CHAN1',
-    ts: '1234.5678',
-  });
   c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :hello @test_slack_bazz in #test_chan_1');
   await c.daemon.onSlackMessage(c.ircUser, {
     text: 'hello <@U1235BAZZ> in <#C1234CHAN1|test_chan_1>',
@@ -108,19 +88,26 @@ test('slack_ircize_text', async(t) => {
 test('slack_ircize_text_backticks', async(t) => {
   t.plan(1 + mocks.connectOneIrcClient.planCount);
   const c = await mocks.connectOneIrcClient(t);
-  c.slackWeb.expect('chat.postMessage', {
-    channel: 'C1234CHAN1',
-    text: 'hello ```<@U1235BAZZ>``` in <#C1234CHAN1|test_chan_1> `user <@U1235BAZZ>` your email is <mailto:aa@bb.cc|not@this.com> winner of `award',
-    as_user: true,
-    thread_ts: null,
-  }, {
-    ok: true,
-    channel: 'C1234CHAN1',
-    ts: '1234.5678',
-  });
   c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :hello ```<@U1235BAZZ>``` in #test_chan_1 `user <@U1235BAZZ>` your email is aa@bb.cc winner of `award');
   await c.daemon.onSlackMessage(c.ircUser, {
     text: 'hello ```<@U1235BAZZ>``` in <#C1234CHAN1|test_chan_1> `user <@U1235BAZZ>` your email is <mailto:aa@bb.cc|not@this.com> winner of `award',
+    user: 'U1234USER',
+    channel: 'C1234CHAN1',
+    ts: '1234.5678',
+  });
+  t.end();
+});
+
+test('slack_ircize_text_backticks_two', async(t) => {
+  t.plan(5 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t);
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :```ERROR:  error one');
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :ERROR:  error two');
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :ERROR:  error three');
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :ERROR:  error four');
+  c.ircSocket.expect(':test_slack_user PRIVMSG #test_chan_1 :DANGER!  ssh <mailto:bob@builder.com|bob@builder.com> bad command had an exit value of: 23```');
+  await c.daemon.onSlackMessage(c.ircUser, {
+    text: '```ERROR:  error one\nERROR:  error two\nERROR:  error three\nERROR:  error four\nDANGER!  ssh <mailto:bob@builder.com|bob@builder.com> bad command had an exit value of: 23```',
     user: 'U1234USER',
     channel: 'C1234CHAN1',
     ts: '1234.5678',
