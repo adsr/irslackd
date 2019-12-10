d0895db5bdcc378d2cfa7523d2b4f0b8bed2bf2d
Adam Saponara <as@php.net>
2019-12-10 05:30:23 +0000

    Fix mpim_open bug

---
+++ tests/test_mpim.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const test = require('tape');
+const mocks = require('./mocks');
+
+test('slack_mpim_open', async(t) => {
+  t.plan(6 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t);
+  c.slackWeb.expect('conversations.info', { channel: 'G1234GROUP' }, {
+    ok: true,
+    channel: {
+      id: 'G1234GROUP',
+      name: 'mpdm-user1--user2--user3--user4-1',
+      topic: { value: 'Group messaging' },
+    },
+  });
+  c.slackWeb.expect('conversations.info', { channel: 'G1234GROUP' }, { // TODO avoid conversations.info twice in mpim_open
+    ok: true,
+    channel: {
+      id: 'G1234GROUP',
+      name: 'mpdm-user1--user2--user3--user4-1',
+      topic: { value: 'Group messaging' },
+    },
+  });
+  c.slackWeb.expect('conversations.members', { channel: 'G1234GROUP', limit: 1000 }, { ok: true, members: [
+    'U1234USER',
+    'U1235BARR',
+    'U1235BAZZ',
+    'U1235QUUX',
+  ]});
+  c.ircSocket.expect(':test_slack_user JOIN #mpdm-user1--user2--user3--user4-1');
+  c.ircSocket.expect(':irslackd 332 test_slack_user #mpdm-user1--user2--user3--user4-1 :Group messaging');
+  c.ircSocket.expect(':irslackd 353 test_slack_user = #mpdm-user1--user2--user3--user4-1 :test_slack_user test_slack_user test_slack_barr test_slack_bazz test_slack_quux');
+  await c.daemon.onSlackMpimOpen(c.ircUser, {
+    user: 'U1234USER',
+    channel: 'G1234GROUP',
+    type: 'mpim_open',
+    is_mpim: true,
+    event_ts: '1234.5678',
+  }, {
+    ts: '1234.5678',
+  });
+  t.end();
+});
