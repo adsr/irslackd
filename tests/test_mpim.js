86d6e944db7a877d4cc40193d1944dd1526973f2
Brett Kosinski <fancypantalons@gmail.com>
2020-06-27 17:44:17 -0400

    Add option to shorten multi-party chat names.
    This commit implements the proposal laid out in issue #96.
    
    By default, multi-party direct message chats are directly mapped to an
    IRC channel name that matches this scheme:
    
      #mpdm-mynick--nick1--nick2--nick3
    
    This is pretty unwieldly.  To try and make these channel names a bit
    more manageable, this changeset adds a new preference:
    
      short-group-chat-names
    
    If this is supplied with the login token, these chat names are instead
    mapped to this scheme:
    
      &nick1-nick2-nick3
    
    This uses the server local chat prefix to differentiate between normal
    and group chats, removes the current user nick, and then takes out
    some extra hyphens to compress the whole thing down.
    
    This is made optional through a preference so as not to disrupt the
    current UX.

--- tests/test_mpim.js
+++ tests/test_mpim.js
@@ -10,7 +10,7 @@ test('slack_mpim_open', async(t) => {
     ok: true,
     channel: {
       id: 'G1234GROUP',
-      name: 'mpdm-user1--user2--user3--user4-1',
+      name: 'mpdm-test_slack_user--user2--user3--user4-1',
       topic: { value: 'Group messaging' },
     },
   });
@@ -18,7 +18,7 @@ test('slack_mpim_open', async(t) => {
     ok: true,
     channel: {
       id: 'G1234GROUP',
-      name: 'mpdm-user1--user2--user3--user4-1',
+      name: 'mpdm-test_slack_user--user2--user3--user4-1',
       topic: { value: 'Group messaging' },
     },
   });
@@ -28,10 +28,52 @@ test('slack_mpim_open', async(t) => {
     'U1235BAZZ',
     'U1235QUUX',
   ]});
-  c.ircSocket.expect(':test_slack_user JOIN #mpdm-user1--user2--user3--user4-1');
-  c.ircSocket.expect(':irslackd 332 test_slack_user #mpdm-user1--user2--user3--user4-1 :Group messaging');
-  c.ircSocket.expect(':irslackd 353 test_slack_user = #mpdm-user1--user2--user3--user4-1 :test_slack_user test_slack_user test_slack_barr test_slack_bazz test_slack_quux');
-  c.ircSocket.expect(':irslackd 366 test_slack_user #mpdm-user1--user2--user3--user4-1 :End of /NAMES list');
+  c.ircSocket.expect(':test_slack_user JOIN #mpdm-test_slack_user--user2--user3--user4-1');
+  c.ircSocket.expect(':irslackd 332 test_slack_user #mpdm-test_slack_user--user2--user3--user4-1 :Group messaging');
+  c.ircSocket.expect(':irslackd 353 test_slack_user = #mpdm-test_slack_user--user2--user3--user4-1 :test_slack_user test_slack_user test_slack_barr test_slack_bazz test_slack_quux');
+  c.ircSocket.expect(':irslackd 366 test_slack_user #mpdm-test_slack_user--user2--user3--user4-1 :End of /NAMES list');
+  await c.daemon.onSlackMpimOpen(c.ircUser, {
+    user: 'U1234USER',
+    channel: 'G1234GROUP',
+    type: 'mpim_open',
+    is_mpim: true,
+    event_ts: '1234.5678',
+  }, {
+    ts: '1234.5678',
+  });
+  c.end();
+  t.end();
+});
+
+test('slack_short_group_chat_names', async(t) => {
+  t.plan(7 + mocks.connectOneIrcClient.planCount);
+  const c = await mocks.connectOneIrcClient(t, ['short-group-chat-names']);
+  c.slackWeb.expect('conversations.info', { channel: 'G1234GROUP' }, {
+    ok: true,
+    channel: {
+      id: 'G1234GROUP',
+      name: 'mpdm-test_slack_user--user2--user3--user4-1',
+      topic: { value: 'Group messaging' },
+    },
+  });
+  c.slackWeb.expect('conversations.info', { channel: 'G1234GROUP' }, { // TODO avoid conversations.info twice in mpim_open
+    ok: true,
+    channel: {
+      id: 'G1234GROUP',
+      name: 'mpdm-test_slack_user--user2--user3--user4-1',
+      topic: { value: 'Group messaging' },
+    },
+  });
+  c.slackWeb.expect('conversations.members', { channel: 'G1234GROUP', limit: 1000 }, { ok: true, members: [
+    'U1234USER',
+    'U1235BARR',
+    'U1235BAZZ',
+    'U1235QUUX',
+  ]});
+  c.ircSocket.expect(':test_slack_user JOIN &user2-user3-user4-1');
+  c.ircSocket.expect(':irslackd 332 test_slack_user &user2-user3-user4-1 :Group messaging');
+  c.ircSocket.expect(':irslackd 353 test_slack_user = &user2-user3-user4-1 :test_slack_user test_slack_user test_slack_barr test_slack_bazz test_slack_quux');
+  c.ircSocket.expect(':irslackd 366 test_slack_user &user2-user3-user4-1 :End of /NAMES list');
   await c.daemon.onSlackMpimOpen(c.ircUser, {
     user: 'U1234USER',
     channel: 'G1234GROUP',
